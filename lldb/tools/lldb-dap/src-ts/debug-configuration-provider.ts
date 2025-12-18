deefc848bf11669adaa0354d4e8e12d154e9db9a
Gabriele Mondada <gab@ijk.ch>
2025-12-18 17:05:34 +0100

    [lldb-dap] implement Android device selection

--- lldb/tools/lldb-dap/src-ts/debug-configuration-provider.ts
+++ lldb/tools/lldb-dap/src-ts/debug-configuration-provider.ts
@@ -7,6 +7,7 @@ import { ConfigureButton, showErrorMessage } from "./ui/show-error-message";
 import { ErrorWithNotification } from "./ui/error-with-notification";
 import { LogFilePathProvider } from "./logging";
 import { ApkDebugConfiguration } from "./android/apk-debug-configuration";
+import { AndroidConfigurationBuilder } from "./android/android-configuration-builder";
 
 const exec = util.promisify(child_process.execFile);
 
@@ -236,9 +237,12 @@ export class LLDBDapConfigurationProvider
           !debugConfiguration.launchCommands ||
           debugConfiguration.launchCommands.length === 0
         ) {
-          // TODO: manage deviceId
+          if (!debugConfiguration.androidDeviceSerial) {
+            debugConfiguration.androidDeviceSerial = await AndroidConfigurationBuilder.resolveDeviceSerial(debugConfiguration.androidDevice);
+            console.log(`Android device serial number: ${debugConfiguration.androidDeviceSerial}`);
+          }
           debugConfiguration.launchCommands =
-            ApkDebugConfiguration.getLldbLaunchCommands(undefined, debugConfiguration.androidComponent);
+            ApkDebugConfiguration.getLldbLaunchCommands(debugConfiguration.androidDeviceSerial, debugConfiguration.androidComponent);
         }
       }
 
